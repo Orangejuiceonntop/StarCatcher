cd6e8419e2b0af1cbbd7e1d6fb402556a06f0c8c
orangejuiceontop <susselkikijimenez@gmail.com>
2026-07-05 15:36:46 -0500

    Add code to first minigame

---
+++ Scenes/star.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var player: CharacterBody2D = $"../Player"
+@onready var self_area = $Area2D
+@onready var player_area = $"../Player/Area2D"
+
+signal star_collected
+
+func _process(delta: float) -> void:
+	if player_area.overlaps_area(self_area):
+		if self.visible:
+			emit_signal("star_collected")
+			self.hide()
