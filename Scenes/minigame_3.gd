28b7f36c1981b147b775d5efa6b69dbabfba2f9b
orangejuiceontop <susselkikijimenez@gmail.com>
2026-07-19 00:29:22 -0500

    Make clicking black hole subtract a life

---
+++ Scenes/minigame_3.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@onready var themed_timer: Node2D = $ThemedTimer
+
+var stars_pressed = 0
+var timer_end = false
+
+
+func _ready() -> void:
+	await themed_timer.Timer(20.0)
+	timer_end = true
+
+func _process(_delta: float) -> void:
+	if stars_pressed == 5:
+		if Global.minigames_done > 3:
+			get_tree().change_scene_to_file("res://Scenes/done_screen.tscn")
+		else:
+			get_tree().change_scene_to_file("res://Scenes/level_scene.tscn")
+	if timer_end:
+		Global.lives -= 1
+		Global.minigames_done -= 1
+		get_tree().change_scene_to_file("res://Scenes/level_scene.tscn")
+
+
+func _on_black_hole_pressed() -> void:
+	Global.lives -= 1
+	Global.minigames_done -= 1
+	get_tree().change_scene_to_file("res://Scenes/level_scene.tscn")
