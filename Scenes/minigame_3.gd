4b8233d6c3ddce6f78f4328e7894363ad8571f80
orangejuiceontop <susselkikijimenez@gmail.com>
2026-07-22 01:54:42 -0500

    Add code to star button

--- Scenes/minigame_3.gd
+++ Scenes/minigame_3.gd
@@ -2,16 +2,17 @@ extends Node2D
 
 @onready var themed_timer: Node2D = $ThemedTimer
 
-var stars_pressed = 0
+var star_clicked = 0
 var timer_end = false
 
 
 func _ready() -> void:
 	await themed_timer.Timer(20.0)
 	timer_end = true
+	
 
 func _process(_delta: float) -> void:
-	if stars_pressed == 5:
+	if star_clicked == 5:
 		if Global.minigames_done > 3:
 			get_tree().change_scene_to_file("res://Scenes/done_screen.tscn")
 		else:
@@ -26,3 +27,9 @@ func _on_black_hole_pressed() -> void:
 	Global.lives -= 1
 	Global.minigames_done -= 1
 	get_tree().change_scene_to_file("res://Scenes/level_scene.tscn")
+
+
+func _on_star_pressed() -> void:
+	$"Star/AnimationPlayer".play("click")
+	star_clicked += 1
+	
